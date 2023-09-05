1d5670ae0910739966f63b3515f394ac392258b0
YanSouza <yan017247@gmail.com>
2023-09-05 19:08:43 -0300

    Entrega 17/09

--- src/algotimosversionado/Aulas/Semana01/Entrega1.java
+++ src/algotimosversionado/Aulas/Semana01/Entrega1.java
@@ -1,7 +1,7 @@
 
-package algotimosversionado.Aulas.PWMASemana01;
+package algotimosversionado.Aulas.Semana01;
 
-public class tentivaB {
+public class Entrega1 {
     public static void main(String[] args) {
         System.out.println("AGR SIM eu acho");
     }
