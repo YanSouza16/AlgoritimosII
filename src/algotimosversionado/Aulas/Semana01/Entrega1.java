4bc425afe5bcec194e9d7c0f9199cb4a323076b7
YanSouza <yan017247@gmail.com>
2023-09-16 11:04:13 -0300

    Trabalho 17/09

--- src/algotimosversionado/Aulas/Semana01/Entrega1.java
+++ src/algotimosversionado/Aulas/Semana01/Entrega1.java
@@ -24,17 +24,20 @@ public class Entrega1 {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int option1, numberMatch, option;
+        boolean numberChecked;
+        int firstOption, index, option;
         int[] vetor = new int[0];
-        do {
-            imprimeMenu();
-            option1 = teclado.nextInt();
-        } while (option1 != 1);
+       do {
+           
+           imprimeMenu();
+            firstOption = teclado.nextInt();
+        } while (firstOption != 1);
 
-        if (option1 == 1) {
+        if (firstOption == 1) {
             vetor = initVetor();
         }
         do {
+            
             imprimeMenu();
             option = teclado.nextInt();
             switch (option) {
@@ -43,30 +46,37 @@ public class Entrega1 {
                     break;
 
                 case 3:
-                    System.out.println("Qual numero deseja buscar:");
+                    System.out.print("Qual numero deseja buscar:");
                     int numberX = teclado.nextInt();
-                    numberMatch = checkNumber(vetor, numberX);
-                    if (numberMatch == 1) {
-                        System.out.print(numberX + "  foi encontrado ");
+                    index = checkNumber(vetor, numberX);
+
+                    if (index == -1) {
+                             System.out.printf("Nao Foi encontrado: %d\n", numberX);
                     } else {
-                        System.out.printf("d% nao Foi encontrado.\n", numberMatch);
+                        System.out.printf("%d  Foi encontrado O indice e: %d \n", numberX, index);
                     }
                     break;
 
                 case 4:
+                    System.out.printf("Maior numero e: %d \n", majorNumber(vetor));
+
                     break;
 
                 case 5:
-
+                    System.out.printf("Percentual: %d \n", avaregeEven(vetor));
                     break;
 
                 case 6:
+                    System.out.printf("Percentual: %d \n", avaregeOdd(vetor));
                     break;
-
                 case 7:
-                    break;
 
+                    break;
                 case 8:
+                    System.out.print("Qual numero deseja somar :\n");
+                    numberX = teclado.nextInt();
+                    numberChecked = sumNumbers(vetor, numberX);
+                    System.out.printf("%b%n   ",numberChecked);
                     break;
                 case 9:
                     System.out.print("\n Encerrando..\n");
@@ -79,8 +89,8 @@ public class Entrega1 {
         } while (option != 9);
 
     }
-    //inicia vetor com tamanho informado, e numeros aleatorios
 
+    //1.inicia vetor com tamanho informado, e numeros aleatorios
     public static int[] initVetor() {
         int m;
         Scanner teclado = new Scanner(System.in);
@@ -96,6 +106,80 @@ public class Entrega1 {
         return vetor;
     }
 
+    //2. imprimir
+    public static void imprimeVetor(int[] vetor) {
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(vetor[i]);
+        }
+    }
+
+    //3. checar numero contido no vetor
+    public static int checkNumber(int vetor[], int numberX) {
+        int index = -1;
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] == numberX) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    //4. Maior numero requesito 
+    public static int majorNumber(int vetor[]) {
+        int major = vetor[0];
+        for (int i = 0; i < vetor.length; i++) {
+            major = Math.max(major, vetor[i]);
+        }
+
+        return major;
+    }
+
+    //media impares
+    public static int avaregeOdd(int vetor[]) {
+        int avarege = 0;
+        for (int i = 0; i < vetor.length; i++) {
+            if (i % 2 != 0) {
+                avarege++;
+            }
+
+        }
+        avarege = avarege / avarege;
+        return avarege * 100;
+    }
+
+    //media pares
+    public static int avaregeEven(int vetor[]) {
+        int avarege = 0;
+        for (int i = 0; i < vetor.length; i++) {
+            if (i % 2 == 0) {
+                avarege++;
+            }
+
+        }
+        avarege = avarege / avarege;
+        return avarege * 100;
+    }
+
+    //8. Soma dos numeros distintos
+    public static boolean sumNumbers(int vetor[], int numberX) {
+        int sum=0;
+       boolean checked = false;
+            for (int i = 0; i < vetor.length; i++) {
+                if(sum!=numberX){
+                       
+                sum += vetor[i];
+                   }else{
+                       sum++;
+                   }
+                
+            }
+         
+        checked = (sum == numberX) ? true : false;
+        return checked;
+    }
+                                                                                                    
+    //Função a parte para melhor reutilização do codigo
     public static void imprimeMenu() {
         System.out.print("Menu:");
         System.out.print("\n1.Inicializar o vetor com numero aleatorios");
@@ -108,28 +192,5 @@ public class Entrega1 {
         System.out.print("\n8.dois numero em posicoes distintas ");
         System.out.print("\n9. Fechar programa ");
         System.out.print("\n(Informe a Opcao desejada com seu numero) \n");
-    }
-
-    public static void imprimeVetor(int[] vetor) {
-
-        for (int i = 0; i < vetor.length; i++) {
-            System.out.println(vetor[i]);
-        }
-    }
-    // Procura numero contido no vetor
-
-    public static int checkNumber(int vetor[], int numberX) {
-        Scanner teclado = new Scanner(System.in);
-
-        for (int i = 0; i < vetor.length; i++) {
-            if (vetor[i] == numberX) {
-                numberX = 1;
-            } else {
-                numberX = -1;
-            }
-        }
-
-        int checked = numberX;
-        return checked;
     }
 }
